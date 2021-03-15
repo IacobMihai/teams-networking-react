dd71f668add864edfb7909ed9dfa1158a8bb43ae
Iacob <mihai.iacob13@yahoo.com>
2021-03-15 21:39:50 +0200

    table added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
 import './App.css';
+import  { PersonsTable } from "./PersonsTable"
+
+let persons = [
+  {
+      "id": "a123",
+      "firstName": "Rares",
+      "lastName": "Roman",
+      "github": "https://github.com/romanrares"
+  },
+  {
+      "id": "b654",
+      "firstName": "Nicolae",
+      "lastName": "Matei",
+      "github": "http://nmatei.github.io/"
+  }
+];
 
 function App() {
   return (
     <div>
       <h1>Team Networking</h1> 
       <div>Search</div>
-      <p>table...</p>
+      <PersonsTable teams={teams} />
     </div>
   );
 }
